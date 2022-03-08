9920ac6291994ab30e5875d7d31faf61e0f5b167
Kezziah C. Ayuno <ayuno@ualberta.ca>
2022-03-07 23:36:22 -0700

    Removed top action bar and also removed irrelevant code

--- app/src/main/java/com/bigyoshi/qrhunt/MainActivity.java
+++ app/src/main/java/com/bigyoshi/qrhunt/MainActivity.java
@@ -23,14 +23,9 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
     }
 
