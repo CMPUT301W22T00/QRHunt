227405b49f7ff80cdd7f7bc3ca3374f564fdfd45
Allan Ma <88dark.allan@gmail.com>
2022-03-09 00:59:16 -0700

    Add and Delete a QR from QRLibrary
    Also added some getters and setters to ExternalQR and  a getter to Player

--- app/src/main/java/com/bigyoshi/qrhunt/ExternalQRCode.java
+++ app/src/main/java/com/bigyoshi/qrhunt/ExternalQRCode.java
@@ -1,7 +1,8 @@
 package com.bigyoshi.qrhunt;
 
 public class ExternalQRCode extends QRCode {
-    private int value;
+    private String id; // Hash of the actual data from the scan
+    private int value; // The score of the QR code
     private int latitude;
     private int longitude;
     private int numScanned;
@@ -9,15 +10,34 @@ public class ExternalQRCode extends QRCode {
 
     // We need to distinguish QRCodes already scanned and those who have not been scanned yet
     //  Since initialization of numScanned would either be an update OR just 1
-    public ExternalQRCode(int longitude, int latitude, int value){
+    public ExternalQRCode(int longitude, int latitude, int value, String id){
         this.value = value;
         this.longitude = longitude;
         this.latitude = latitude;
+        this.id = id;
+        this.numScanned = 1;
     }
+
+    // Just a bunch of getters and setters
+    public int getNumScanned() { return this.numScanned; }
 
     public void updateNumScanned(){
         this.numScanned += 1;
     }
 
+    public int getValue() { return this.value; }
 
+    public void setValue(int value) { this.value = value; }
+
+    public int getLatitude() { return latitude; }
+
+    public void setLatitude(int latitude) { this.latitude = latitude; }
+
+    public int getLongitude() { return longitude; }
+
+    public void setLongitude(int longitude) { this.longitude = longitude; }
+
+    public String getId() { return id; }
+
+    public void setId(String id) { this.id = id; }
 }
