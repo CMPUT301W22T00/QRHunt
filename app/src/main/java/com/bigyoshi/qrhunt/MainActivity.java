1d31e4a6eb7758101caf3dd11def9c5980763376
Kezziah C. Ayuno <ayuno@ualberta.ca>
2022-03-09 20:04:27 -0700

    Just added a top bar to help with flow

--- app/src/main/java/com/bigyoshi/qrhunt/MainActivity.java
+++ app/src/main/java/com/bigyoshi/qrhunt/MainActivity.java
@@ -2,8 +2,12 @@ package com.bigyoshi.qrhunt;
 
 
 import android.Manifest;
+import android.app.ActionBar;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
