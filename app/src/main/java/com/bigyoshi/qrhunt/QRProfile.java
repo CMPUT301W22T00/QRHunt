df8c961cc70009adbca16e92298e59a166f5e6a3
QueenBeta <zznnila11@gmail.com>
2022-03-11 00:54:08 -0700

    icons

---
+++ app/src/main/java/com/bigyoshi/qrhunt/QRProfile.java
@@ -0,0 +1,14 @@
+package com.bigyoshi.qrhunt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QRProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qrprofile);
+    }
+}
