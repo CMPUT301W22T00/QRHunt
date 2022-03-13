6ee8efe6b9c41d9e4fa204e6a1b8a29979c96ff5
Allan Ma <88dark.allan@gmail.com>
2022-03-12 19:19:48 -0700

    Update AugmentedCamera.java

--- app/src/main/java/com/bigyoshi/qrhunt/AugmentedCamera.java
+++ app/src/main/java/com/bigyoshi/qrhunt/AugmentedCamera.java
@@ -13,6 +13,7 @@ import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentManager;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -83,6 +84,7 @@ public class AugmentedCamera {
         if (ActivityCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             return;
         }
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(activity);
         fusedLocationClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
             @Override
             public void onComplete(@NonNull Task<Location> task) {
