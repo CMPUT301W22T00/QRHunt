3a083b39f5908dc27188192e2cdc5365c1758081
Kezziah C. Ayuno <ayuno@ualberta.ca>
2022-03-14 14:17:55 -0600

    testing stuff

--- app/src/androidTest/java/com/bigyoshi/qrhunt/FragmentScannerTest.java
+++ app/src/androidTest/java/com/bigyoshi/qrhunt/FragmentScannerTest.java
@@ -1,4 +1,32 @@
 package com.bigyoshi.qrhunt;
 
+import android.app.Activity;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
+
+import com.robotium.solo.Solo;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
 public class FragmentScannerTest {
+
+    private Solo solo;
+
+    @Rule
+    public ActivityTestRule rule = new ActivityTestRule(FragmentScanner.class,
+            true, true);
+
+    @Before
+    public void setUp() throws Exception {
+        solo = new Solo(InstrumentationRegistry.getInstrumentation(), rule.getActivity());
+    }
+
+    @Test
+    public void start() throws Exception {
+        Activity activity = rule.getActivity();
+    }
+
 }
