241b8649a6b34c1a0cb4f85341c2f91f566c0c4f
QueenBeta <zznnila11@gmail.com>
2022-03-14 15:14:35 -0600

    Added some missing javadoc descriptions in FragmentAddQRCode and partially added some descriptions on UnplayableQRCode

--- app/src/main/java/com/bigyoshi/qrhunt/UnplayableQRCode.java
+++ app/src/main/java/com/bigyoshi/qrhunt/UnplayableQRCode.java
@@ -1,21 +1,21 @@
 package com.bigyoshi.qrhunt;
 
 /**
- * Definition:
+ * Definition: Generates a QR code with the account specifications to access the account on other devices
  *
  *
  */
 public class UnplayableQRCode {
 
     /**
-     *
+     * Constructor
      */
     public UnplayableQRCode(){
     }
 
     /**
      *
-     * @return
+     * @return true
      */
     public Boolean isGameStatus(){
         // True if it is game status, false then it is LogIn; could have a isLogIn for completeness though
@@ -23,14 +23,14 @@ public class UnplayableQRCode {
     }
 
     /**
-     *
+     *  Gets game status features and displays it in a view (if getter method - delete description)
      */
     public void getGameStatusInfo(){
         // Function used to get game status features and displaying it in a view (UI)
     }
 
     /**
-     *
+     * Gets accessing account or "log-in" specifications (if getter method - delete description)
      */
     public void getLogInInfo(){
         // Function used to get log-in specifications
