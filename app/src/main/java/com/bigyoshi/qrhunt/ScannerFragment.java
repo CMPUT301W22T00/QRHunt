124c451309ccd85b28e93c3a31e8b4cefa6d419d
Allan Ma <88dark.allan@gmail.com>
2022-03-14 00:07:35 -0600

    Changer CameraScannerFragment to ScannerFragment

--- app/src/main/java/com/bigyoshi/qrhunt/ScannerFragment.java
+++ app/src/main/java/com/bigyoshi/qrhunt/ScannerFragment.java
@@ -12,7 +12,6 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import com.bigyoshi.qrhunt.databinding.FragmentMapBinding;
 import com.budiyev.android.codescanner.AutoFocusMode;
 import com.budiyev.android.codescanner.CodeScanner;
 import com.budiyev.android.codescanner.CodeScannerView;
@@ -21,14 +20,14 @@ import com.budiyev.android.codescanner.ErrorCallback;
 import com.budiyev.android.codescanner.ScanMode;
 import com.google.zxing.Result;
 
-import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
-import org.osmdroid.views.MapView;
-import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
-import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
-
-public class CameraScannerFragment extends Fragment {
+public class ScannerFragment extends Fragment {
     private CodeScanner codeScanner;
     public AugmentedCamera camera;
+    private String playerId;
+
+    public ScannerFragment(String playerId) {
+        this.playerId = playerId;
+    }
 
     @Nullable
     @Override
@@ -56,7 +55,7 @@ public class CameraScannerFragment extends Fragment {
                     public void run() {
                         //Leave this here for now, but will need to remove later
                         Toast.makeText(activity, result.getText(), Toast.LENGTH_SHORT).show();
-                        camera = new AugmentedCamera(CameraScannerFragment.this, result.getText());
+                        camera = new AugmentedCamera(ScannerFragment.this, result.getText());
                         codeScanner.setScanMode(ScanMode.PREVIEW);
                         camera.processQRCode();
                         Toast.makeText(activity, "QR ADDED", Toast.LENGTH_SHORT).show();
