34deb950fe73d6dc5b000733baa0a1862a1f1d6d
Anya Hanatuke <anyahanatuke@gmail.com>
2022-03-14 22:52:58 -0600

    SearchFragment.java didn't need work, AugmentedCamera.java is done

--- app/src/main/java/com/bigyoshi/qrhunt/AugmentedCamera.java
+++ app/src/main/java/com/bigyoshi/qrhunt/AugmentedCamera.java
@@ -29,7 +29,7 @@ public class AugmentedCamera {
     private final Fragment frag;
     private final String qrContent;
     private byte[] digest;
-    private LocationCallback hackyLocationCallback;
+    private LocationCallback hackyLocationCallback; //todo rename this maybe
     FusedLocationProviderClient client;
     public String hash;
     private String playerId;
@@ -55,20 +55,26 @@ public class AugmentedCamera {
      *
      */
     public void pollLocation() {
-        // alex please forgive me
-        // super, super hacky way to get location not to be null.
-        // apparently just requesting it is enough to get the location manager off it's ass
-        // even if we never care about the result
-        // it's super messed up
-        // if it break in the future, we may need to implement more of LocationCallback
-        // but it's working now
-        // https://stackoverflow.com/questions/29441384/fusedlocationapi-getlastlocation-always-null/29854418#29854418
+        /* alex please forgive me
+           super, super hacky way to get location not to be null.
+           apparently just requesting it is enough to get the location manager off it's ass
+           even if we never care about the result
+           it's super messed up
+           if it break in the future, we may need to implement more of LocationCallback
+           but it's working now
+           https://stackoverflow.com/questions/29441384/fusedlocationapi-getlastlocation-always-null/29854418#29854418
+            */
         LocationRequest mLocationRequest = LocationRequest.create();
         mLocationRequest.setInterval(10 * 1000);
         mLocationRequest.setFastestInterval(1000);
         mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
         hackyLocationCallback = new LocationCallback() {};
-        if (ActivityCompat.checkSelfPermission(frag.getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(frag.getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        if (ActivityCompat.checkSelfPermission(frag.getActivity(),
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(frag.getActivity(),
+                Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
             return;
         }
         client.requestLocationUpdates(mLocationRequest, hackyLocationCallback, null);
@@ -92,12 +98,18 @@ public class AugmentedCamera {
                 if (location != null) {
                     qrCode.setLocation(location.getLatitude(), location.getLongitude());
                 }
-                // this stops listening to the updates we didn't actually care about in the first place for
-                // see pollLocation
-                LocationServices.getFusedLocationProviderClient(frag.getActivity()).removeLocationUpdates(hackyLocationCallback);
+                /* this stops listening to the updates that
+                we didn't actually care about in the first place for; see pollLocation
+                 */
+                LocationServices.getFusedLocationProviderClient(
+                        frag.getActivity()).removeLocationUpdates(hackyLocationCallback);
                 // todo: do we really need a whole wrapper class?
                 // todo: handle cases where we can get the location gracefully
-                new FragmentAddQRCode(hash, score, new QRLocation(location.getLatitude(), location.getLongitude()), playerId).show(frag.getChildFragmentManager(), "ADD QR");
+                new FragmentAddQRCode(hash,
+                        score,
+                        new QRLocation(location.getLatitude(), location.getLongitude()),
+                        playerId).show(frag.getChildFragmentManager(),
+                        "ADD QR");
             }
         });
     }
@@ -107,19 +119,19 @@ public class AugmentedCamera {
      *
      */
     private void computeHash() {
-        MessageDigest md = null;
+        MessageDigest messageDigest = null;
         try {
-            md = MessageDigest.getInstance("SHA-1");
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
+            messageDigest = MessageDigest.getInstance("SHA-1");
+        } catch (NoSuchAlgorithmException noAlgEx) {
+            noAlgEx.printStackTrace();
         }
-        assert md != null;
-        digest = md.digest(qrContent.getBytes());
-        StringBuilder sb = new StringBuilder();
+        assert messageDigest != null;
+        digest = messageDigest.digest(qrContent.getBytes());
+        StringBuilder stringBuilder = new StringBuilder();
         for (byte b : digest) {
-            sb.append(Integer.toString((b & 0xff) + 0x100, 16).substring(1));
+            stringBuilder.append(Integer.toString((b & 0xff) + 0x100, 16).substring(1));
         }
-        hash = sb.toString();
+        hash = stringBuilder.toString();
     }
 
     /**
@@ -127,10 +139,14 @@ public class AugmentedCamera {
      *
      */
     private void computeScore() {
-        // Need to calculate score here
-        // Probably have to pass in the hash or whatever we use to calculate the value
-        // sha1 gives 160 bits → max value is therefore 2^160
-        score = new BigInteger(1, digest).multiply(new BigInteger("100")).divide((new BigInteger("2").pow(160))).intValue();
+        /* Need to calculate score here
+           Probably have to pass in the hash or whatever we use to calculate the value
+           sha1 gives 160 bits → max value is therefore 2^160
+            */
+        score = new BigInteger(1, digest)
+                .multiply(new BigInteger("100"))
+                .divide((new BigInteger("2").pow(160)))
+                .intValue();
     }
 
     /**
@@ -139,6 +155,7 @@ public class AugmentedCamera {
      */
     private void capturePhoto() {
         // Saves the photo and correlates it to QRProfile
+        //Todo finish capturePhoto
     }
 
     /**
@@ -147,7 +164,10 @@ public class AugmentedCamera {
      * @return client.getLastLocation()
      */
     private Task<Location> getLocation() {
-        if (ActivityCompat.checkSelfPermission(frag.getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(frag.getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        if (ActivityCompat.checkSelfPermission(frag.getActivity(),
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(frag.getActivity(),
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             return null;
         }
         return client.getLastLocation();
