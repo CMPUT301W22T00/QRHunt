18320b2177a188fd904204277f2eef12b370701d
Anya Hanatuke <anyahanatuke@gmail.com>
2022-04-05 03:15:44 -0600

    progress

---
+++ app/src/androidTest/java/com/bigyoshi/qrhunt/FragmentQRLibraryTest.java
@@ -0,0 +1,45 @@
+package com.bigyoshi.qrhunt;
+
+import android.app.Activity;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
+
+import com.robotium.solo.Solo;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class FragmentQRLibraryTest {
+    private Solo solo;
+
+    public void goToPlayerProfile(){
+        solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
+        solo.clickOnView(solo.getCurrentActivity().findViewById(R.id.top_navigation_profile));
+
+    }
+
+    @Rule
+    public ActivityTestRule rule = new ActivityTestRule(MainActivity.class,
+            true, true);
+
+    @Before
+    public void setUp() throws Exception {
+        solo = new Solo(InstrumentationRegistry.getInstrumentation(), rule.getActivity());
+    }
+
+    @Test
+    public void start() throws Exception {
+        Activity activity = rule.getActivity();
+    }
+
+    //test works assuming exactly one qr code w/ no image
+
+    public void testQrLibrary(){
+        goToPlayerProfile();
+
+        solo.clickOnImage();
+    }
+
+}
