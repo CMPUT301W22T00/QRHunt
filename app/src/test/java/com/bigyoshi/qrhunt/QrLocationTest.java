2dff564dcea130d243e553e79c213b63120e82cd
niazieo <omar.niazie@gmail.com>
2022-04-05 01:35:42 -0600

    QrLocation tests

--- app/src/test/java/com/bigyoshi/qrhunt/QrLocationTest.java
+++ app/src/test/java/com/bigyoshi/qrhunt/QrLocationTest.java
@@ -1,8 +1,37 @@
 package com.bigyoshi.qrhunt;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.bigyoshi.qrhunt.qr.QrLocation;
+
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 public class QrLocationTest {
+    private QrLocation mockQrLocation() {
+        QrLocation location = new QrLocation();
+        location.setLatitude(53);
+        location.setLong(-113);
+        return location;
+    }
+
+    @Test
+    void testGetLongitude(){
+        QrLocation location = mockQrLocation();
+        location.setLong(113);
+        assertEquals("113.0", String.valueOf(location.getLongitude()));
+    }
+
+    @Test
+    void testGetLatitude(){
+        QrLocation location = mockQrLocation();
+        location.setLatitude(51);
+        assertEquals("51.0", String.valueOf(location.getLatitude()));
+    }
+
+    @Test
+    void testGetGeoHash(){
+        QrLocation location = mockQrLocation();
+        assertEquals(location.getGeoHash(), mockQrLocation().getGeoHash());
+    }
+
 }
