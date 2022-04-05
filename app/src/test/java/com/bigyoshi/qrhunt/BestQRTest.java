5d0e23ab353d8131b1a01b8f433598362afa4d02
Kezziah C. Ayuno <ayuno@ualberta.ca>
2022-04-05 01:15:15 -0600

    Junit stuff

--- app/src/test/java/com/bigyoshi/qrhunt/BestQRTest.java
+++ app/src/test/java/com/bigyoshi/qrhunt/BestQRTest.java
@@ -1,4 +1,42 @@
 package com.bigyoshi.qrhunt;
 
+import com.bigyoshi.qrhunt.player.BestQr;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
 public class BestQRTest {
+    private BestQr mockBestQRR(){
+        BestQr mockBestQR = new BestQr();
+        mockBestQR.setQrId("TEST-T00");
+        mockBestQR.setScore(999);
+        return mockBestQR;
+    }
+
+    @Test
+    void testGetQrId(){
+        BestQr mockBestQr = mockBestQRR();
+        assertEquals("TEST-T00", mockBestQr.getQrId());
+    }
+
+    @Test
+    void testSetQrId(){
+        BestQr mockBestQr = mockBestQRR();
+        mockBestQr.setQrId("TEST-T002");
+        assertEquals("TEST-T002", mockBestQr.getQrId());
+    }
+
+    @Test
+    void testGetScore(){
+        BestQr mockBestQr = mockBestQRR();
+        assertEquals(999, mockBestQr.getScore());
+    }
+
+    @Test
+    void testSetScore(){
+        BestQr mockBestQr = mockBestQRR();
+        mockBestQr.setScore(0);
+        assertEquals(0, mockBestQr.getScore());
+    }
+
 }
